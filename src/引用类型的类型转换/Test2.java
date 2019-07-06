8ea7ba816fbc5b05b66961b1c3f04dcdd126b58a
yanbo0039 <yanbo0039@yeah.net>
2019-07-06 10:13:55 +0800

    Java高级—多态及产生条件

--- src/引用类型的类型转换/Test2.java
+++ src/引用类型的类型转换/Test2.java
@@ -5,10 +5,23 @@ public class Test2 {
 	public static void main(String[] args) {
 //这里不能调用cry()方法，因为将其上溯造型成咯Animal，
 //Animal里有有 这方法才有，Animal里没有就 没有
-		Animal a = new Cat();
+//		Animal a = new Cat();
+//		
+//		Cat c = (Cat) a;//下溯造型
+//		
+//		c.cry();
 		
-		Cat c = (Cat) a;//下溯造型
 		
-		c.cry();
+		
+		Animal a = new Animal();
+		a.eat();
+		Animal b = new Cat();
+		b.eat();
+		Animal c = new Dog();
+		c.eat();
+		/**
+		 * 调用相同的方法，其行为是不一样的行为
+		 * 
+		 */
 	}
 }
